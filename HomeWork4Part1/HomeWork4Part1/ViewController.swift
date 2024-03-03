f1c90639d27de5f6d5721cdedf29e5abe77ce11f
oleksandr-muza <131881080+oleksandr-muza@users.noreply.github.com>
2024-03-03 16:10:02 +0200

    add UIButton / IBOutlet / IBAction

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -11,14 +11,25 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var texrLable: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         texrLable.text = "Hello iOS!"
-
+        firstButton.backgroundColor = .purple
+        firstButton.layer.cornerRadius = 50
+        firstButton.configuration?.attributedTitle = "Welcome button"
+        firstButton.tintColor = .white
+        
     }
 
 
+    @IBAction func firstButtonAction(_ sender: UIButton) {
+        texrLable.text = "Hi!"
+        
+    }
+    
+    
 }
 
