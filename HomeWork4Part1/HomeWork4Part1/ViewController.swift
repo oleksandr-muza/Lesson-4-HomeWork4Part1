ff074a97366434a8361beec599325a343302e358
oleksandr-muza <131881080+oleksandr-muza@users.noreply.github.com>
2024-03-03 15:37:27 +0200

    add UILable / IBOutlet / value

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var texrLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        texrLable.text = "Hello iOS!"
+
     }
 
 
